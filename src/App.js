ef0558ca72d49d810b55a69d3f05079fcce9055f
joao <joao@gertel.com.br>
2024-02-28 14:16:33 -0300

    TEste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, onValue } from 'firebase/database';
+import React, { useState } from 'react';
 import './App.css';
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
 
 function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyB97mPfrzfnDl0yZds1FDdULWafkECqTNE",
+    authDomain: "chat-gertel.firebaseapp.com",
+    databaseURL: "https://chat-gertel-default-rtdb.firebaseio.com",
+    projectId: "chat-gertel",
+    storageBucket: "chat-gertel.appspot.com",
+    messagingSenderId: "321918378253",
+    appId: "1:321918378253:web:6b2029f9e1ae44014bd044"
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase();
+
+  
+function teste(param) {
+  const db = getDatabase();
+  set(ref(db, '/GrupoId'), {
+    teste: param
+  });
+}
+
+teste('Teste')
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>
+        <h1>teste</h1>
+      </div>
       </header>
     </div>
   );
 }
+
 
 export default App;
